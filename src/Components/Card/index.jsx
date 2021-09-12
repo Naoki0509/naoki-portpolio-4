3368d59a4349b6db97c106e81c7e7e77fd452017
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-09-13 03:10:28 +0900

    add code

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,15 +1,29 @@
 import { Box } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/image";
+import { Stack, HStack, VStack } from "@chakra-ui/react";
 
 export function Card() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full">
-			<div className="">
-				<div>
-					<Box w={} bg="tomato" color="white" py={4}>
-						HELL
+			<Box
+				w="100%"
+				h="100%"
+				bg="black"
+				border="1px solid"
+				borderColor="gray.200"
+				rounded="lg"
+				p="4"
+				textColor="white"
+			>
+				<Stack direction={["colum", "row"]} spacing="24px">
+					<Box>
+						<h1>Image</h1>
 					</Box>
-				</div>
-			</div>
+					<Box>
+						<h1>Card</h1>
+					</Box>
+				</Stack>
+			</Box>
 		</div>
 	);
 }
