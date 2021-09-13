35d57d0684e304fb09368651a7940943d09acf5b
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-09-13 16:41:08 +0900

    add code

--- src/Components/Blog/Home/Card/index.jsx
+++ src/Components/Blog/Home/Card/index.jsx
@@ -4,9 +4,16 @@ export function Card() {
 	return (
 		<div className="flex items-center justify-center w-full">
 			<div>
-				<Box bg="gray.100" p={4} width="100%">
-					<h2>Card</h2>
-				</Box>
+				<div>
+					<Box h="100%" w="100%" bg="gray.200">
+						<Box>
+							<h1>Image</h1>
+						</Box>
+						<Box>
+							<h1>Card</h1>
+						</Box>
+					</Box>
+				</div>
 			</div>
 		</div>
 	);
