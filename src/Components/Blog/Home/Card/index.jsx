75093be92bdce8fb8e320dc07dabdbe62b5457cd
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-09-19 20:44:18 +0900

    add

--- src/Components/Blog/Home/Card/index.jsx
+++ src/Components/Blog/Home/Card/index.jsx
@@ -1,13 +1,7 @@
-import { Center, Square, Circle, Box } from "@chakra-ui/react";
-
 export function Card() {
 	return (
-		<div className="flex items-center justify-center">
-			<div>
-				<div className="">
-					<Box></Box>
-				</div>
-			</div>
+		<div class="flex justify-center items-center">
+			<div></div>
 		</div>
 	);
 }
